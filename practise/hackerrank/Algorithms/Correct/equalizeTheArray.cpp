69f76269dd6a4d4c285233e4c9cbbea853371ef7
Tito <teetoegeonu@gmail.com>
2020-07-08 18:15:59 +0100

    ADD- Solved equalizeTheArray challange on hackerrank

---
+++ practise/hackerrank/Algorithms/Correct/equalizeTheArray.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the equalizeArray function below.
+int equalizeArray(vector<int> arr) {
+    int n = arr.size();
+    
+    //vector <vector<int>> outer;
+    set <vector<int>> s;
+    for(int i = 0; i < n; i++) {
+        vector <int> v;
+        v.push_back(arr[i]);
+        v.push_back(count(arr.begin(), arr.end(), arr[i]));
+        
+        s.insert(v);
+    }
+    
+    int num;
+    int count;
+    int best = 0;
+    int bestNum;
+    
+   
+
+    for(auto i: s) {
+        int incre = 0;
+        for(auto j : i) {
+            if(incre == 0) {
+                num = j;
+            }
+            
+            if(incre == 1) {
+                count = j;
+            }
+            incre++;
+        }
+        
+        
+        if(count > best) {
+            best = count;
+            bestNum = num;
+        }
+    }
+    
+    
+    
+    
+    return n - best;
+}
+
+int main()
+{
+    
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+    
+    vector<string> arr_temp = split_string(arr_temp_temp);
+    
+    vector<int> arr(n);
+    
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+        
+        arr[i] = arr_item;
+    }
+    
+    int result = equalizeArray(arr);
+    
+    cout << result << "\n";
+    
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+    
+    input_string.erase(new_end, input_string.end());
+    
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+    
+    vector<string> splits;
+    char delimiter = ' ';
+    
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+    
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+        
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+    
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+    
+    return splits;
+}
