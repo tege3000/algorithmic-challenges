9e7c439f3565e60e5b420268bd5009adb5961a36
Tito <teetoegeonu@gmail.com>
2020-07-13 12:55:01 +0100

    ADD- Solved fairRations challange on hackerrank

---
+++ practise/hackerrank/Algorithms/Correct/fairRations.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the fairRations function below.
+string fairRations(vector<int> B) {
+    int ans = 0;
+    int n = B.size();
+    
+    for(int i = 0; i < n-1; i++) {
+        if(!(B[i] % 2 == 0)) {
+            B[i] = B[i] + 1;
+            B[i+1] = B[i+1] +1;
+            ans = ans+2;
+            
+        }
+    }
+    
+    int odd = 0;
+    for(int i = 0; i < n; i++) {
+        if(!(B[i] % 2 == 0)) {
+            odd++;
+        }
+    }
+    
+    if(odd > 0) {
+        return "NO";
+    }
+    else {
+        return to_string(ans);
+    }
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int N;
+    cin >> N;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string B_temp_temp;
+    getline(cin, B_temp_temp);
+
+    vector<string> B_temp = split_string(B_temp_temp);
+
+    vector<int> B(N);
+
+    for (int i = 0; i < N; i++) {
+        int B_item = stoi(B_temp[i]);
+
+        B[i] = B_item;
+    }
+
+    string result = fairRations(B);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
