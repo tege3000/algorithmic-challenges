0fb0eeb49d49df70ce20c2850dcfee7bd7677720
Tito <teetoegeonu@gmail.com>
2020-07-24 17:25:52 +0100

    UPD - Added second trial for twoCharacters challange on hackerrank

--- practise/hackerrank/Algorithms/Strings/Wrong/twoCharacters.cpp
+++ practise/hackerrank/Algorithms/Strings/Wrong/twoCharacters.cpp
@@ -7,94 +7,101 @@ string rtrim(const string &);
 
 // Complete the alternate function below.
 int alternate(string s) {
-    string newS = s;
-    sort(newS.begin(), newS.end());
-    cout << newS << endl;
-    string alphabets = "abcdefghijklmnopqrstuvwxyz";
-
-    auto it = find(alphabets.begin(), alphabets.end(), newS[0]);
-    int count = 1;
-
-    //vector<vector<char, int>> outer;
-    vector <pair<char,int>> outer;
-    for(int i  = 1; i < newS.size(); i++) {
-        if(newS[i] == *it) {
-            count++;
-        }
-        else {
+    set <char> distinct;
+    int n = s.size();
+    for(int i = 0 ; i < n; i++) {
+        distinct.insert(s[i]);
+    }
+    
+//    for(auto i : distinct) {
+//        cout << i <<  " ";
+//    }
+//    cout << "\n";
+    
+    vector <char> v;
+    
+    for(auto i : distinct) {
+        v.push_back(i);
+    }
+    
+    vector <vector<char>> outer;
+    
+    for(int i = 0; i < v.size(); i++) {
+        for(int j = i+1; j < v.size(); j++) {
+            vector <char> inner;
+            inner.push_back(v[i]);
+            inner.push_back(v[j]);
             
-            if(i == newS.size() - 1) {
-                //cout << *it <<  " " << count << endl; // print the second to the last one
-                outer.push_back(std::make_pair(*it,count));
-                
-                it = find(alphabets.begin(), alphabets.end(), newS[i]);
-                count = 1;
-                
-                //cout << *it <<  " " << count << endl; // print the last one
-                outer.push_back(std::make_pair(*it,count));
+            outer.push_back(inner);
+        }
+    }
+    
+//    for(auto i : outer) {
+//        for(auto j : i) {
+//            cout << j << " ";
+//        }
+//        cout << "\n";
+//    }
+    
+    string nS;
+    int counter;
+    int length = 0;
+    
+    for(int i = 0; i < outer.size(); i++) {
+        nS = s;
+        //cout << nS << endl;
+        counter = 0;
+        for(int j = 0; j < outer.size(); j = counter) {
+            if(nS[j] == outer[i][0]) {
+                nS.erase(nS.begin()+j);
+                counter = j;
+            }
+            else if(nS[j] == outer[i][1]) {
+                nS.erase(nS.begin()+j);
+                counter = j;
             }
             else {
-                //cout << *it <<  " " << count << endl;
-                outer.push_back(std::make_pair(*it,count));
-                
-                it = find(alphabets.begin(), alphabets.end(), newS[i]);
-                count = 1;
+                counter++;
             }
             
         }
-    }
-    
+        
+        //cout << nS << endl;
+        char first = nS[0];
+        char second = nS[1];
+        int eCheck = 0;
+        int oCheck = 0;
+        for(int i = 0; i < nS.size(); i = i+2) {
+            if(first == nS[i]) {
+                eCheck = 1;
 
-    int max = 0;
-    char firstEl;
-    
-    
-    for (auto i : outer) {
-        if(i.second > max) {
-            max = i.second;
-            firstEl = i.first;
-            
+            }
+            else {
+                eCheck = 0;
+                break;
+            }
         }
         
-        cout << i.first << " " << i.second << endl;
-    }
-    
-    cout << max << endl;
-    
-    int least = max;
-    char leastEl;
-    
-    for (auto i : outer) {
-        if(i.second < least) {
-            least = i.second;
-            leastEl = i.first;
+        for(int i = 1; i < nS.size(); i = i+2) {
+            if(second == nS[i]) {
+                oCheck = 1;
+            }
+            else {
+                oCheck = 0;
+                break;
+            }
         }
         
-    }
-    
-    
-    cout << least << endl;
-    
-    cout << firstEl << endl;
-    cout << leastEl << endl;
-    
-    cout << s << endl;
-    string resS;
-    
-    for(int i = 0; i < s.size(); i++) {
-        //cout << i+1 << ": " << s[i] << endl;
-        if(s[i] == firstEl || s[i] == leastEl) {
-            //s.erase(s.begin()+i);
-            continue;
-        }
-        else {
-            resS.push_back(s[i]);
+        if(eCheck == 1 && oCheck == 1){
+            //cout << nS << " is valid" << endl;
+            if(nS.size() > length) {
+                length = nS.size();
+            }
         }
     }
+
     
-    cout << resS << endl;
-    
-    return resS.size();
+    return length;
     
 }
 
