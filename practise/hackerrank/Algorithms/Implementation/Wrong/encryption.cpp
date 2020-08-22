525647c9cb252b3747e29c18ae3600654c883321
Tito <teetoegeonu@gmail.com>
2020-08-22 17:17:23 +0100

    UPD - second implementation of encryption challange on hackerrank, this actually works but for some weird reason its not passing the test cases. But the return values are the same. Very weird

--- practise/hackerrank/Algorithms/Implementation/Wrong/encryption.cpp
+++ practise/hackerrank/Algorithms/Implementation/Wrong/encryption.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <math.h>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -13,44 +11,63 @@ string encryption(string s) {
     cout << a << " " << b << endl;
     
     vector<string> rewritten;
+    
     string t;
     int start = 0;
-    for(int i = 0; i < l; i++) {
+    int size  =0;
+    if(a*b < l) {
+        if(a<b) {
+            a = b;
+        }
+        else {
+            b = a;
+        }
+    }
+
+    for(int i = 0; i < a*b; i++) {
         t.push_back(s[i]);
+
         start++;
-        if(start == b || i == l-1) {
+        if(start == b) {
             start = 0;
             rewritten.push_back(t);
             t= "";
         }
     }
     
+    
+    
     for(int i  = 0; i < rewritten.size(); i++) {
         cout << rewritten[i] << endl;
     }
     
-    string res;
+    
+    
+    string res="";
     for(int i = 0; i < rewritten[0].size(); i++) {
         for(int j = 0; j < rewritten.size(); j++) {
             res.push_back(rewritten[j][i]);
+
         }
         
-        if(i != rewritten[0].size() - 1) {
-            res.push_back(' ');
-        }
+        res.push_back(' ');
     }
-    
+
     return res;
 }
 
 int main()
 {
+    ofstream fout(getenv("OUTPUT_PATH"));
+
     string s;
     getline(cin, s);
-    
+
     string result = encryption(s);
-    
-    cout << result << "\n";
-    
+
+    fout << result << "\n";
+
+    fout.close();
+
     return 0;
 }
