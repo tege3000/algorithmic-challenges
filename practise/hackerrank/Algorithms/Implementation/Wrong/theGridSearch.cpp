d3af863ea4d2db17e6ab6503c5f5f4deae9b276f
Tito <teetoegeonu@gmail.com>
2020-08-23 17:38:53 +0100

    UPD - debugging theGridSearch challange

--- practise/hackerrank/Algorithms/Implementation/Wrong/theGridSearch.cpp
+++ practise/hackerrank/Algorithms/Implementation/Wrong/theGridSearch.cpp
@@ -1,26 +1,32 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 vector<string> split_string(string);
 
 // Complete the gridSearch function below.
-string gridSearch(vector<string> G, vector<string> P, int r) {
-    int n = G.size();
+string gridSearch(vector<string> G, vector<string> P, int r, int R) {
+    int n = G[0].size();
     int m = P[0].size();
 //    cout << n << " " << m << endl;
     string k, kNew;
-    vector <string> test;
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < R; i++) {
 //        cout << "Row " << i+1 << endl;
-        for(int j = 0; j < n-m; j++) {
+        for(int j = 0; j < n-m+1; j++) {
+            
             k = "";
             for(int a = j; a < j+m; a++) {
                 k.push_back(G[i][a]);
 //                cout << G[i][a] << " ";
             }
-            cout << "\n";
+            //cout << "\n";
+            
+            //cout << k << endl;
             if(k == P[0]) {
+                vector <string> test;
+
+                cout << "entered " << i << j << endl;
                 for(int x = i; x < i+r; x++) {
                     kNew = "";
                     for(int y = j; y < j+m; y++) {
@@ -29,7 +35,14 @@ string gridSearch(vector<string> G, vector<string> P, int r) {
                     test.push_back(kNew);
                 }
                 
+//                for(auto e: test) {
+//                    for(auto f : e) {
+//                        cout << f << " ";
+//                    }
+//                    cout << "\n";
+//                }
                 
+                cout << "\n";
                 if(test == P) {
                     return "YES";
                 }
@@ -44,56 +57,54 @@ string gridSearch(vector<string> G, vector<string> P, int r) {
 
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
-
+    
     int t;
     cin >> t;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
+    
     for (int t_itr = 0; t_itr < t; t_itr++) {
         string RC_temp;
         getline(cin, RC_temp);
-
+        
         vector<string> RC = split_string(RC_temp);
-
+        
         int R = stoi(RC[0]);
-
+        
         int C = stoi(RC[1]);
-
+        
         vector<string> G(R);
-
+        
         for (int i = 0; i < R; i++) {
             string G_item;
             getline(cin, G_item);
-
+            
             G[i] = G_item;
         }
-
+        
         string rc_temp;
         getline(cin, rc_temp);
-
+        
         vector<string> rc = split_string(rc_temp);
-
+        
         int r = stoi(rc[0]);
-
+        
         int c = stoi(rc[1]);
-
+        
         vector<string> P(r);
-
+        
         for (int i = 0; i < r; i++) {
             string P_item;
             getline(cin, P_item);
-
+            
             P[i] = P_item;
         }
-
-        string result = gridSearch(G, P, r);
-
-        fout << result << "\n";
+        
+        string result = gridSearch(G, P, r, R);
+        
+        cout << result << "\n";
     }
-
-    fout.close();
-
+    
+    
     return 0;
 }
 
@@ -101,27 +112,27 @@ vector<string> split_string(string input_string) {
     string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
         return x == y and x == ' ';
     });
-
+    
     input_string.erase(new_end, input_string.end());
-
+    
     while (input_string[input_string.length() - 1] == ' ') {
         input_string.pop_back();
     }
-
+    
     vector<string> splits;
     char delimiter = ' ';
-
+    
     size_t i = 0;
     size_t pos = input_string.find(delimiter);
-
+    
     while (pos != string::npos) {
         splits.push_back(input_string.substr(i, pos - i));
-
+        
         i = pos + 1;
         pos = input_string.find(delimiter, i);
     }
-
+    
     splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
+    
     return splits;
 }
