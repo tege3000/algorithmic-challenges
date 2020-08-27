0df932682dae8d2148d6f973e17919f239942fd4
Tito <teetoegeonu@gmail.com>
2020-08-27 17:23:53 +0100

    UPD - added much better approach for solving absolutePermutation problem on hackerrank. However, its still not passing some test cases. Currently passes 6/13 test cases

--- practise/hackerrank/Algorithms/Implementation/Wrong/absolutePermutation.cpp
+++ practise/hackerrank/Algorithms/Implementation/Wrong/absolutePermutation.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -14,19 +13,18 @@ vector<int> absolutePermutation(int n, int k) {
     
     // swap first k elements with next k elements and so on
     if(k != 0) {
-        for(int i = 0; i < n; i = k*2) {
+        for(int i = 0; i < n; i += k*2) {
             
             
-            for(int j = 0; j < k; j++) {
-                int tmp = perms[j];
-                perms[j] = perms[j+k];
-                perms[j+k] = tmp;
+            for(int j = i; j < i+k; j++) {
+                swap(perms[j], perms[j+k]);
             }
             
         }
     }
     
     
+    // check if correct
     for(int i = 0; i < perms.size(); i++) {
         if(abs(perms[i] - (i+1)) == k && perms[i] != 0) {
             continue;
@@ -37,42 +35,43 @@ vector<int> absolutePermutation(int n, int k) {
         }
 
     }
-  
-    return perms;
     
+    return perms;
 }
 
 int main()
 {
-    
+    ofstream fout(getenv("OUTPUT_PATH"));
+
     int t;
     cin >> t;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    
+
     for (int t_itr = 0; t_itr < t; t_itr++) {
         string nk_temp;
         getline(cin, nk_temp);
-        
+
         vector<string> nk = split_string(nk_temp);
-        
+
         int n = stoi(nk[0]);
-        
+
         int k = stoi(nk[1]);
-        
+
         vector<int> result = absolutePermutation(n, k);
-        
+
         for (int i = 0; i < result.size(); i++) {
-            cout << result[i];
-            
+            fout << result[i];
+
             if (i != result.size() - 1) {
-                cout << " ";
+                fout << " ";
             }
         }
-        
-        cout << "\n";
+
+        fout << "\n";
     }
-    
-    
+
+    fout.close();
+
     return 0;
 }
 
@@ -80,27 +79,27 @@ vector<string> split_string(string input_string) {
     string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
         return x == y and x == ' ';
     });
-    
+
     input_string.erase(new_end, input_string.end());
-    
+
     while (input_string[input_string.length() - 1] == ' ') {
         input_string.pop_back();
     }
-    
+
     vector<string> splits;
     char delimiter = ' ';
-    
+
     size_t i = 0;
     size_t pos = input_string.find(delimiter);
-    
+
     while (pos != string::npos) {
         splits.push_back(input_string.substr(i, pos - i));
-        
+
         i = pos + 1;
         pos = input_string.find(delimiter, i);
     }
-    
+
     splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-    
+
     return splits;
 }
