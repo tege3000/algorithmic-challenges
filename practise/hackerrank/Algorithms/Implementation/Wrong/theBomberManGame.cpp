a202e704f51423c97649b83b47bbcba42cab7176
Tito <teetoegeonu@gmail.com>
2020-08-28 19:50:19 +0100

    UPD - debugged previous approach, and now atleast passing 7/266 test cases

--- practise/hackerrank/Algorithms/Implementation/Wrong/theBomberManGame.cpp
+++ practise/hackerrank/Algorithms/Implementation/Wrong/theBomberManGame.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -63,7 +62,7 @@ vector<string> bomberMan(int n, vector<string> grid) {
                 grid[x-1][y] = '.';
             }
             
-            if(x != row) {
+            if(x != row-1) {
                 grid[x+1][y] = '.';
             }
             
@@ -71,7 +70,7 @@ vector<string> bomberMan(int n, vector<string> grid) {
                 grid[x][y-1] = '.';
             }
             
-            if(y != col) {
+            if(y != col-1) {
                 grid[x][y+1] = '.';
             }
             
@@ -89,39 +88,42 @@ vector<string> bomberMan(int n, vector<string> grid) {
 
 int main()
 {
-    
+    ofstream fout(getenv("OUTPUT_PATH"));
+
     string rcn_temp;
     getline(cin, rcn_temp);
-    
+
     vector<string> rcn = split_string(rcn_temp);
-    
+
     int r = stoi(rcn[0]);
-    
+
     int c = stoi(rcn[1]);
-    
+
     int n = stoi(rcn[2]);
-    
+
     vector<string> grid(r);
-    
+
     for (int i = 0; i < r; i++) {
         string grid_item;
         getline(cin, grid_item);
-        
+
         grid[i] = grid_item;
     }
-    
+
     vector<string> result = bomberMan(n, grid);
-    
+
     for (int i = 0; i < result.size(); i++) {
-        cout << result[i];
-        
+        fout << result[i];
+
         if (i != result.size() - 1) {
-            cout << "\n";
+            fout << "\n";
         }
     }
-    
-    cout << "\n";
-    
+
+    fout << "\n";
+
+    fout.close();
+
     return 0;
 }
 
@@ -129,27 +131,27 @@ vector<string> split_string(string input_string) {
     string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
         return x == y and x == ' ';
     });
-    
+
     input_string.erase(new_end, input_string.end());
-    
+
     while (input_string[input_string.length() - 1] == ' ') {
         input_string.pop_back();
     }
-    
+
     vector<string> splits;
     char delimiter = ' ';
-    
+
     size_t i = 0;
     size_t pos = input_string.find(delimiter);
-    
+
     while (pos != string::npos) {
         splits.push_back(input_string.substr(i, pos - i));
-        
+
         i = pos + 1;
         pos = input_string.find(delimiter, i);
     }
-    
+
     splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-    
+
     return splits;
 }
