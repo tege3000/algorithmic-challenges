352f02050b25420b7b800d1c7b4f7b8b3adc376a
Tito <teetoegeonu@gmail.com>
2020-08-29 20:40:18 +0100

    UPD - second attempt. 19/23 test cases failing

--- practise/hackerrank/Algorithms/Implementation/Wrong/emasSupercomputer.cpp
+++ practise/hackerrank/Algorithms/Implementation/Wrong/emasSupercomputer.cpp
@@ -7,10 +7,69 @@ vector<string> split_string(string);
 // Complete the twoPluses function below.
 int twoPluses(vector<string> grid) {
     vector<int> areas;
+    vector<vector<int>> avoidOverlap;
+    int count = 0;
+    int moveon = 0;
     for(int i = 0; i <= grid.size() - 3; i++) {
         for(int j = 0; j <= grid[0].size()-3; j++) {
+            
             if(grid[i][j+1] == 'G' && grid[i+1][j+1] == 'G' && grid[i+2][j+1] == 'G' && grid[i+1][j] == 'G' && grid[i+1][j+1] == 'G' && grid[i+1][j+2] == 'G') {
-                areas.push_back(5);
+                if(count <= 0) {
+                    areas.push_back(5);
+                    
+                    vector<int> index1;
+                    index1.push_back(i);
+                    index1.push_back(j+1);
+                    avoidOverlap.push_back(index1);
+                    vector<int> index2;
+                    index2.push_back(i+2);
+                    index2.push_back(j+1);
+                    avoidOverlap.push_back(index2);
+                    vector<int> index3;
+                    index3.push_back(i+1);
+                    index3.push_back(j);
+                    avoidOverlap.push_back(index3);
+                    vector<int> index4;
+                    index4.push_back(i+1);
+                    index4.push_back(j+2);
+                    avoidOverlap.push_back(index4);
+                    vector<int> index5;
+                    index5.push_back(i+1);
+                    index5.push_back(j+1);
+                    avoidOverlap.push_back(index5);
+                }
+                else {
+                    
+                    for(int a = 0; a < avoidOverlap.size(); a++) {
+//                        cout << "current: "<< i  << " " << j+1 << " old: " << avoidOverlap[a][0] << " " << avoidOverlap[a][1] << endl;
+//                        cout << "current: "<< i+2  << " " << j+1 << " old: " << avoidOverlap[a][0] << " " << avoidOverlap[a][1] << endl;
+//                        cout << "current: "<< i+1  << " " << j << " old: " << avoidOverlap[a][0] << " " << avoidOverlap[a][1] << endl;
+//                        cout << "current: "<< i+1  << " " << j+2 << " old: " << avoidOverlap[a][0] << " " << avoidOverlap[a][1] << endl;
+//                        cout << "current: "<< i+1  << " " << j+1 << " old: " << avoidOverlap[a][0] << " " << avoidOverlap[a][1] << endl;
+//                        cout << "\n";
+                        
+                        if((i == avoidOverlap[a][0] && j+1 == avoidOverlap[a][1]) ||
+                           (i+2 == avoidOverlap[a][0] && j+1 == avoidOverlap[a][1]) ||
+                           (i+1 == avoidOverlap[a][0] && j == avoidOverlap[a][1]) ||
+                           (i+1 == avoidOverlap[a][0] && j+2 == avoidOverlap[a][1]) ||
+                           (i+1 == avoidOverlap[a][0] && j+1 == avoidOverlap[a][1])) {
+                            moveon = 1;
+                        }
+                    }
+                    
+                    if(moveon == 1) {
+                        moveon = 0;
+                        continue;
+                    }
+                    else {
+                        areas.push_back(5);
+
+                    }
+                    
+                }
+                count++;
+                
+                
             }
             
             
