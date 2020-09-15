5cc52b52c3eaa26313df80e733a3dc0fe70a6e88
Tito <teetoegeonu@gmail.com>
2020-09-15 18:50:10 +0100

    ADD - Implemented algorithm for greedyFlorist challange on hackerrank

---
+++ practise/hackerrank/Algorithms/Greedy/Correct/greedyFlorist.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the getMinimumCost function below.
+int getMinimumCost(int k, vector<int> c) {
+    sort(c.begin(), c.end(), greater<int>());
+    int n = c.size();
+    int cost = 0;
+    int start = 0;
+    for(int i = 0; i < ceil((n*1.0)/k); i++) {
+        for(int j = start; j < k + start; j++) {
+            if(j > n-1) {
+                break;
+            }
+            else {
+                cost += (i+1)* c[j];
+            }
+        }   
+        start += k;
+    }
+
+    return cost;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nk_temp;
+    getline(cin, nk_temp);
+
+    vector<string> nk = split_string(nk_temp);
+
+    int n = stoi(nk[0]);
+
+    int k = stoi(nk[1]);
+
+    string c_temp_temp;
+    getline(cin, c_temp_temp);
+
+    vector<string> c_temp = split_string(c_temp_temp);
+
+    vector<int> c(n);
+
+    for (int i = 0; i < n; i++) {
+        int c_item = stoi(c_temp[i]);
+
+        c[i] = c_item;
+    }
+
+    int minimumCost = getMinimumCost(k, c);
+
+    fout << minimumCost << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
