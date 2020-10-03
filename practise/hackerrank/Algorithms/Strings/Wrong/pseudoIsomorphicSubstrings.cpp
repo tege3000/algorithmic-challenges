6adc1dc0fe58a89db2cfa5484e320dbb7bdaba92
Tito <teetoegeonu@gmail.com>
2020-10-03 19:32:00 +0100

    UPD - Making progress. Implemented first approach at solving challange, but its not producing correct ans, so need to debug

--- practise/hackerrank/Algorithms/Strings/Wrong/pseudoIsomorphicSubstrings.cpp
+++ practise/hackerrank/Algorithms/Strings/Wrong/pseudoIsomorphicSubstrings.cpp
@@ -7,11 +7,18 @@ using namespace std;
 /*
  * Complete the pseudoIsomorphicSubstrings function below.
  */
+
+bool comp(string l, string r) {
+    if(l.size() == r.size()) {
+        return l < r;
+    }
+    return l.size() < r.size();
+}
 vector<int> pseudoIsomorphicSubstrings(string s) {
     /*
      * Write your code here.
      */
-    vector<int> ans = {0};
+    vector<int> ans;
     int n = s.size();
     string c;
     for(int i = 0; i < n; i++) {
@@ -25,16 +32,64 @@ vector<int> pseudoIsomorphicSubstrings(string s) {
             }
         }
         
-        for(auto i : subString) {
-            cout << i << " ";
-        }
-        cout << "\n\n";
+        // CURRENTLY HAVE ALL THE SUBSTRINGS. NEXT STEP IS
+        // TO REMOVE THE PSEUDO ISOMORPHIC SUBSTRINGS FROM THE SET
+        vector<string> subNew;
         
+        for(auto j : subString) {
+            subNew.push_back(j);
+        }
+        
+        
+        sort(subNew.begin(), subNew.end(), comp);
+        int count = subNew.size();
+        cout << "count is  " << count << "\n";
+        for(int j = 0; j < subNew.size(); j++) {
+//            cout << subNew[j] << " ";
+            
+            for(int k = j+1; k < subNew.size(); k++) {
+                if(subNew[j].size() == subNew[k].size()) {
+                    int passCase2And3 = 0;
+                    for(int l = 0; l < subNew[j].size(); l++) {
+                        for(int m = l+1; m < subNew[j].size(); m++) {
+                            if(((subNew[j][l] == subNew[j][m]) && (subNew[k][l] == subNew[k][m]))||((subNew[j][l] != subNew[j][m]) && (subNew[k][l] != subNew[k][m]))) {
+                                passCase2And3 = 1;
+                            }
+                            else {
+                                passCase2And3 = 0;
+                                break;
+                            }
+                        }
+                        
+                        if(passCase2And3 == 0) {
+                            break;
+                        }
+                    }
+                    
+                    // if passes the tests then is a pseudo isomorphic sub string, so we delete it
+                    if(passCase2And3 == 1) {
+                        cout << "cookoo " << "\n";
+                        count--;
+                    }
+                    else {
+                        continue;
+                    }
+                }
+                else {
+                    break;
+                }
+            }
+            
+        }
+        ans.push_back(count);
+        
+//        cout << "\n\n";
 
+        
+        
     }
     
-    // CURRENTLY HAVE ALL THE SUBSTRINGS. NEXT STEP IS 
-    //TO REMOVE THE PSEUDO ISOMORPHIC SUBSTRINGS FROM THE SET
+    
     return ans;
     
 }
