5a86984fd0bfec11a61bfb3caa2b84d5451748d6
Tito <teetoegeonu@gmail.com>
2020-10-19 16:34:03 +0100

    UPD - first implementation was a bit overkill

--- practise/codeForces/rating<1300/Correct/wordCapitalization.cpp
+++ practise/codeForces/rating<1300/Correct/wordCapitalization.cpp
@@ -1,23 +1,9 @@
 #include <iostream>
-#include <sstream>
-#include <vector>
 
 using namespace std;
 int main() {
     string s;
-    
     cin >> s;
-    
-    istringstream iss(s);
-    vector<string> words;
-    
-    for(string w; iss >> w;) {
-        words.push_back(w);
-    }
-    
-    for(int i = 0; i < words.size(); i++) {
-        words[i][0]  = (char)toupper(words[i][0]);
-        cout << words[i] << " ";
-    }
 
+    cout << (char)toupper(s[0]) + s.substr(1, s.size()-1);
 }
